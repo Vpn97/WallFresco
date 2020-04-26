92f264667d8d94390bb6385141691a6c7f00f1ef
Vpn97 <vpn0097@gmail.com>
2020-04-26 13:39:45 +0530

    Implement TrendingFragment view and live data.

--- app/src/main/java/com/apkzube/wallfresco/ui/trending/TrendingFragment.java
+++ app/src/main/java/com/apkzube/wallfresco/ui/trending/TrendingFragment.java
@@ -17,20 +17,56 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.apkzube.wallfresco.R;
+import com.apkzube.wallfresco.adapter.WallpaperAdapter;
 import com.apkzube.wallfresco.databinding.FragmentTrendingBinding;
+import com.apkzube.wallfresco.db.entity.Wallpaper;
 import com.google.android.material.card.MaterialCardView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrendingFragment extends Fragment {
 
     private FragmentTrendingBinding mBinding;
-
-    private TrendingViewModel trendingViewModel;
+    private TrendingViewModel model;
+    private WallpaperAdapter adapter;
+    private ArrayList<Wallpaper> wallpapers;
+    private LottieAnimationView  animationTrending;
+    private RecyclerView rvTrending;
 
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-        trendingViewModel = ViewModelProviders.of(this).get(TrendingViewModel.class);
+        model = ViewModelProviders.of(this).get(TrendingViewModel.class);
         mBinding= DataBindingUtil.inflate(inflater,R.layout.fragment_trending,container,false);
+        mBinding.setModel(model);
+        allocation();
+        setEvent();
         return mBinding.getRoot();
+    }
+
+    private void allocation() {
+        rvTrending=mBinding.rvTrending;
+        animationTrending=mBinding.animationTrending;
+        StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(getResources().getInteger(R.integer.column_count),
+                LinearLayoutManager.VERTICAL);
+        rvTrending.setLayoutManager(manager);
+
+        model.getTrendingWallpaper();
+        wallpapers=new ArrayList<>();
+        adapter=new WallpaperAdapter(mBinding.getRoot().getContext(),wallpapers);
+        rvTrending.setAdapter(adapter);
+    }
+
+    private void setEvent() {
+        model.getTrendingWallpaper().observe(getViewLifecycleOwner(), wallpapersList -> {
+            animationTrending.setVisibility(View.GONE);
+            wallpapers.clear();
+            wallpapers.addAll(wallpapersList);
+        });
     }
 }
--- app/src/main/java/com/apkzube/wallfresco/ui/trending/TrendingViewModel.java
+++ app/src/main/java/com/apkzube/wallfresco/ui/trending/TrendingViewModel.java
@@ -1,19 +1,63 @@
 package com.apkzube.wallfresco.ui.trending;
 
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class TrendingViewModel extends ViewModel {
+import com.apkzube.wallfresco.db.entity.Wallpaper;
+import com.apkzube.wallfresco.response.PelexsResponse;
+import com.apkzube.wallfresco.service.PexelsService;
+import com.apkzube.wallfresco.service.impl.PexelsServiceImpl;
+import com.apkzube.wallfresco.util.CommonRestURL;
+import com.apkzube.wallfresco.util.Constant;
+import com.apkzube.wallfresco.util.ConverterUtil;
+import com.google.gson.Gson;
 
-    private MutableLiveData<String> mText;
+import java.util.ArrayList;
+import java.util.List;
 
-    public TrendingViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class TrendingViewModel extends AndroidViewModel {
+
+    private Application application;
+    private MutableLiveData<List<Wallpaper>> wallpaperLivedata =new MutableLiveData<>();
+
+    public TrendingViewModel(@NonNull Application application) {
+        super(application);
+        this.application=application;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<Wallpaper>> getTrendingWallpaper() {
+        PexelsService service= PexelsServiceImpl.getService();
+        service.getTrendWallpapers(CommonRestURL.getApiKEY(),CommonRestURL.PER_PAGE_WALLPAPER,1)
+                .enqueue(new Callback<PelexsResponse>() {
+                    @Override
+                    public void onResponse(Call<PelexsResponse> call, Response<PelexsResponse> response) {
+                        Log.d(Constant.TAG, "onResponse: "+new Gson().toJson(response.body()));
+
+                        if(null!=response && null!=response.body()) {
+                            ArrayList<Wallpaper> wallpapers = ConverterUtil.convertResponseToEntityList(response.body());
+                            wallpaperLivedata.setValue(wallpapers);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<PelexsResponse> call, Throwable t) {
+                        Log.d(Constant.TAG, "onFailure: "+new Gson().toJson(t));
+                    }
+                });
+        return wallpaperLivedata;
     }
+
+
+
+
 }
